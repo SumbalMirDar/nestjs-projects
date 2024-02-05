b1b9e42db8b25d5a2c5d3baaa568571dc51300fe
SumbalMir786 <sumbal.alphasquad@gmail.com>
2024-02-05 21:34:41 +0500

    validation checks

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,7 +1,7 @@
 import { PrismaService } from "src/prisma.services";
 import {Book} from "./book.model";
 import { Injectable } from "@nestjs/common/decorators";
-
+import { BadRequestException } from '@nestjs/common';
 
 
 @Injectable()
@@ -13,16 +13,26 @@ export class BookService {
 
 
     async getBook(id:number): Promise<Book | null>{
+        if (!id) {
+            throw new BadRequestException('Id required');
+          }
         return this.prisma.book.findUnique({where : {id:Number(id)}})
     }
 
 
     async createBook(data: Book):Promise<Book>{
+        if (!data.title || !data.description) {
+            throw new BadRequestException('Title and description are required fields');
+          }
+        
         return this.prisma.book.create({
             data,
         })
     }
     async updateBook(id:number, data:Book): Promise<Book>{
+        if (!id) {
+            throw new BadRequestException('Id required');
+          }
         return this.prisma.book.update({
             where:{id:Number(id)},
             data:{title:data.title, description:data.description}
