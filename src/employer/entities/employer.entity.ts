084d0f978ecd8a42c78cad3fd9bd6b51a94f7264
SumbalMir786 <sumbal.alphasquad@gmail.com>
2024-05-30 19:24:34 +0500

    jobseeker application in NESTJS

---
+++ src/employer/entities/employer.entity.ts
@@ -0,0 +1,19 @@
+
+export class User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  displayName: string | null;
+  address:string | null;;
+  contact: string | null;
+  designation: string | null;
+  profileImage: string | null;
+  leadText: string | null;
+  lastLogin: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+
+}
